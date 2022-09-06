4b73c90c497edf2c6fa418bf5ceebf301aa2f31e
your_github_username <muhammad.usama12@hotmail.com>
2022-09-06 13:05:23 +0000

    updated

--- express_server.js
+++ express_server.js
@@ -10,10 +10,10 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars)
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
-});
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: "Hello World!" };
+//   res.render("hello_world", templateVars);
+// });
 
 
 
