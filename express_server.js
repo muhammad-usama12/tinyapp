10eaf0615147355cee125b65169af62de4b557e7
your_github_username <muhammad.usama12@hotmail.com>
2022-09-06 22:37:12 +0000

    redirect implemented

--- express_server.js
+++ express_server.js
@@ -4,9 +4,22 @@ const app = express();
 const PORT = 8080;
 
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 function generateRandomString() {}
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
 
 
 app.get('/urls', (req, res) => {
@@ -14,10 +27,11 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars)
 });
 
-app.get('/urls:id', (req, res) => {
-  const templateVars = { id: req.params.id, longUrl: "http://www.lighthouselabs.ca"};
-  res.render("urls_show", templateVars)
-});
+
+// app.get('/urls:id', (req, res) => {
+//   const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca"};
+//   res.render("urls_show", templateVars)
+// });
 
 
 app.get("/urls/new", (req, res) => {
@@ -25,35 +39,14 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+console.log(req.body);
+}); 
+
+app.get("/u/:id", (req, res) => {
+const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
-
-
-
-
-// app.get('/urls:id', (req, res) => {
-//   const templateVars = { id: req.params.id, longUrl: "http://www.google.com" };
-//   res.render("urls_show", templateVars)
-// });
-
-// app.get("/hello", (req, res) => {
-//   const templateVars = { greeting: "Hello World!" };
-//   res.render("hello_world", templateVars);
-// });
-
-
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -62,6 +55,16 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+// app.get('/urls:id', (req, res) => {
+//   const templateVars = { id: req.params.id, longURL: "http://www.google.com" };
+//   res.render("urls_show", templateVars)
+// });
+
+// app.get("/hello", (req, res) => {
+//   const templateVars = { greeting: "Hello World!" };
+//   res.render("hello_world", templateVars);
+// });
 
 // app.get("/hello", (req, res) => {
 //   res.send("<html><body>Hello <b>World</b></body></html>\n");
