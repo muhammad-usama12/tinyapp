994402518cb73bc74872795aa115bfdc7ad2f60d
your_github_username <muhammad.usama12@hotmail.com>
2022-09-12 20:15:13 +0000

    lint and final cleanup

--- express_server.js
+++ express_server.js
@@ -25,29 +25,8 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-const users = {
-  userRandomID: {
-    id: "userRandomID",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  user2RandomID: {
-    id: "user2RandomID",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
-
-const urlDatabase = {
-  b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
-  },
-  i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
-  },
-};
+const users = {};
+const urlDatabase = {};
 
 app.get("/", (req, res) => {
   const currentUser = req.session.user_id;
